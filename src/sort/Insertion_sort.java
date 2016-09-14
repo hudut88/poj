a129147db3d80c0b2fac72b00aedf4ff96b7c84f
jeffofofof <1532444709@qq.com>
2016-09-14 09:18:07 +0800

    RandomUtil

--- src/sort/Insertion_sort.java
+++ src/sort/Insertion_sort.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 public class Insertion_sort {
 
 	public static void main(String[] args) {
-		int[] aInteger = SortUtil.getRanDomIntegers(100);
+		int[] aInteger = SortUtil.getRanDomIntegersArrays(100,300);
 		//int[] aInteger = {2,4,1,3,1,3,7,9,10,8,12,3,5,6};
 		//int[] aInteger ={91, 10, 62, 38, 25, 39, 25, 38, 62, 63, 89, 69, 44, 36, 37, 61, 88, 0, 30, 24, 98, 58, 79, 43, 56, 27, 37, 93, 15, 96, 5};
 		System.out.println(Arrays.toString(aInteger));
@@ -43,21 +43,26 @@ public class Insertion_sort {
 	 */
 	public static void binary_insertion_sort(int[] intArr) {
 		for (int i = 1; i < intArr.length ; i++) {
-			System.out.println("loop-->"+i);
-			if (i == 29) {
-				System.out.println("");
-			}
+			//System.out.println("loop-->"+i);
 			int swap_idx = binaryFindInsertionIdx(intArr ,0, i -1 , i);
-			System.out.println("swap_idx-->"+swap_idx +" "+ i);
+			//System.out.println("swap_idx-->"+swap_idx +" "+ i);
 			executeInsertion(intArr , i , swap_idx);
-			System.out.println(Arrays.toString(intArr));
+			//System.out.println(Arrays.toString(intArr));
 		}
 	}
 
+	/**
+	 * 折半查找插入位置
+	 * @param intArr
+	 * @param bgn
+	 * @param end
+	 * @param i
+	 * @return
+	 */
 	private static int binaryFindInsertionIdx(int[] intArr, int bgn, int end ,int i) {
 		if (bgn <= end) {
 			int middle = bgn + (end - bgn) /2;
-			System.out.println("middle-->"+middle);
+			//System.out.println("middle-->"+middle);
 			if (intArr[i] >= intArr[middle]) {
 				return binaryFindInsertionIdx(intArr , middle + 1, end ,i );
 			}else {
@@ -67,6 +72,12 @@ public class Insertion_sort {
 		return bgn;
 	}
 
+	/**
+	 * 插入操作
+	 * @param intArr
+	 * @param end_idx
+	 * @param j
+	 */
 	private static void executeInsertion(int[] intArr, int end_idx, int j) {
 		if (end_idx == j) {
 			return;
--- src/sort/SortUtil.java
+++ src/sort/SortUtil.java
@@ -6,15 +6,24 @@ import java.util.Random;
 public class SortUtil {
 
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(getRanDomIntegers(50)));
+		System.out.println(Arrays.toString(getRanDomIntegersArrays(50)));
 
 	}
 	
-	public static int[] getRanDomIntegers(Integer size){
+	public static int[] getRanDomIntegersArrays(Integer size){
 		Random random =new Random();
 		int[] intArr = new int[size];
 		for (int i = 0; i < intArr.length; i++) {
 			intArr[i] = random.nextInt(100);
+		}
+		return intArr;
+	}
+	
+	public static int[] getRanDomIntegersArrays(Integer size , Integer range){
+		Random random =new Random();
+		int[] intArr = new int[size];
+		for (int i = 0; i < intArr.length; i++) {
+			intArr[i] = random.nextInt(range);
 		}
 		return intArr;
 	}
