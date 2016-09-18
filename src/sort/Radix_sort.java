0d2738f3b577ef4ec691405eb8fc091570ecc6ea
jeffofofof <1532444709@qq.com>
2016-09-18 18:00:01 +0800

    2016年9月18日 17:59:59

--- src/sort/Radix_sort.java
+++ src/sort/Radix_sort.java
@@ -1,6 +1,8 @@
 package sort;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 /**
  * »ùÊýÅÅÐò
  * @author ypf
@@ -9,6 +11,7 @@ import java.util.Arrays;
 public class Radix_sort {
 
 	public static void main(String[] args) {
+		Queue<Integer>  queue= new LinkedList();
 		int[] aInteger = {50,123,543,187,49,30,0,2,11,100};
 		sort(aInteger);
 		System.out.println(Arrays.toString(aInteger));
@@ -16,6 +19,11 @@ public class Radix_sort {
 
 	public static void sort(int[] intArr){
 		int radix = 10;
+		int length = intArr.length;
+		int 
+		for (int i = 0; i < length; i++) {
+			intArr[i] / radix;
+		}
 	}
 
 	private static void swap(int[] intArr, int i, int j) {
